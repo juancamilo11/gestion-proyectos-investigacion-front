ff5e98b3beadd6300c0f53ca043d04f6aea2e6c1
Juan Camilo Cardona Calderon <juan.cardona69@udea.edu.co>
2022-03-14 21:18:25 -0500

    Se completó la página del login con estilos

--- src/pages/login/LoginPage.jsx
+++ src/pages/login/LoginPage.jsx
@@ -1,18 +1,64 @@
 import React from "react";
 import { useDispatch } from "react-redux";
 import { startGoogleLogin } from "../../actions/authActions";
+import pickRandomImage from "../../helpers/login/rangomImageGenerator";
 
 const LoginPage = () => {
   const dispatch = useDispatch();
 
-  const handleLogin = (e) => {
+  const handleLoginWithGoogle = (e) => {
     e.preventDefault();
     dispatch(startGoogleLogin());
   };
 
   return (
-    <div>
-      <button onClick={handleLogin}>Login</button>
+    <div className="login__main-container">
+      <div className="login__sidebar">
+        <div className="login__welcome-section">
+          <img
+            src="https://res.cloudinary.com/dahwtwzdl/image/upload/v1647310126/gestion-proyectos-investigacion/logosimbolo-vertical_coeexo.png"
+            className="login__welcome-section-img"
+            alt="logo"
+          />
+          <h3 className="login__welcome-section-title">
+            El más completo administrador de proyectos de investigación de la
+            Universidad de Antioquia
+          </h3>
+        </div>
+        <div className="login__login-buttons-section">
+          <button
+            onClick={handleLoginWithGoogle}
+            className="login__button-login login__button-google"
+          >
+            <img
+              src={
+                process.env.PUBLIC_URL +
+                "/assets/img/login/google-img-button.png"
+              }
+              className="login__login-buttons-img"
+              alt="google-login"
+            />
+            <span className="login__login-button-text">
+              Ingresar con Google
+            </span>
+          </button>
+        </div>
+      </div>
+      <div
+        className="login__welcome-img-container"
+        style={{
+          backgroundImage: `url(${pickRandomImage()})`,
+        }}
+      >
+        <section className="login__app-description">
+          <h3 className="login__app-description-title">
+            Administración de Proyectos de Investigación
+          </h3>
+          <h5 className="login__app-description-title">
+            Universidad de Antioquia
+          </h5>
+        </section>
+      </div>
     </div>
   );
 };

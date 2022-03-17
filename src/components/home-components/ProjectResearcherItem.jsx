63544c0fab4e4ab08a53c659514ce5018db53e1b
Juan Camilo Cardona Calderon <juan.cardona69@udea.edu.co>
2022-03-17 13:48:16 -0500

    Se realizó la lista de investigadores del proyecto

--- src/components/home-components/ProjectResearcherItem.jsx
+++ src/components/home-components/ProjectResearcherItem.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { createRoutesFromChildren } from "react-router-dom";
 
 const ProjectResearcherItem = ({
   id,
@@ -9,25 +10,36 @@ const ProjectResearcherItem = ({
   dateOfEntry,
   role,
   career,
-  name,
-  code,
 }) => {
-  return <div> </div>;
+  const handleDeleteResearcher = (e) => {
+    e.preventDefault();
+  };
+  return (
+    <div>
+      <div className="project-form__objective-item">
+        <img src={photoURL} className="project-form__researcher-photo" />
+        <p>
+          <b>-</b> {displayName}
+        </p>
+        <p>{email}</p>
+        <p>
+          {career.name} - {career.code}
+        </p>
+        <p className="project-form__researcher-role">
+          {role === "RESEARCH_LEADER" ? "Líder Investigador" : "Investigador"}
+        </p>
+
+        <div className="project-form-specific-objective-info">
+          <button
+            className="project-form__delete-specific-objective"
+            onClick={handleDeleteResearcher}
+          >
+            <i className="fas fa-trash-alt "></i>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ProjectResearcherItem;
-
-/*
-{
-  "id": "97M7VMD-M8C5MX34-5M3475",
-  "displayName": "Pedrito Perez Arango",
-  "email": "pedrito.perez@udea.edu.co",
-  "photoURL": "https://i.pinimg.com/originals/ae/8a/c2/ae8ac2fa217d23aadcc913989fcc34a2.png",
-  "phoneNumber": "3122555499",
-  "dateOfEntry": "2020-05-10",
-  "role": "RESEARCH_LEADER",
-  "career": {
-    "name": "Ing. Química",
-    "code": "513"
-  }
-*/

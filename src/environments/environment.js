1d9701761f2245b98cd7788dced76fb827950d7a
Juan Camilo Cardona Calderon <juan.cardona69@udea.edu.co>
2022-03-20 18:06:44 -0500

    Cambio de la url del environment apuntando al back

--- src/environments/environment.js
+++ src/environments/environment.js
@@ -1,2 +1,2 @@
-export const urlBase = "http://localhost:8080/api/v1";
-// export const urlBase = "https://gestion-proyectos-inv-udea.herokuapp.com/api/v1";
+// export const urlBase = "http://localhost:8080/api/v1";
+export const urlBase = "https://gestion-proyectos-udea.herokuapp.com/api/v1";

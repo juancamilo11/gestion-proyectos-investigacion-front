643f57d78c53bbd36a5cbcd61e2f55ce53d7258c
Johan Sebastian Muentes Casadiego <johan.muentes@udea.edu.co>
2022-03-20 12:20:59 -0500

    Se trabajó en los componentes de los objetivos en la vista de un proyecto

---
+++ src/components/home-components/ProjectSpecificObjectiveListReadOnly.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import ErrorFlag from "../ui/ErrorFlag";
+import ProjectSpecificObjectiveItemReadOnly from "./ProjectSpecificObjectiveItemReadOnly";
+
+const ProjectSpecificObjectiveListReadOnly = ({ specificObjectives }) => {
+  return (
+    <div className="project-form_specific-objectives-container project-view_specific-objectives-container">
+      <div>
+        <div>
+          {specificObjectives.length === 0 ? (
+            <ErrorFlag
+              message="Este proyecto aún no tiene objetivos específicos ingresados.
+              uno"
+              color="#f54d4d"
+              fontColor="white"
+              width="95%"
+            />
+          ) : (
+            <h3 className="project-form__specific-objectives-title">
+              Lista de objetivos específicos
+            </h3>
+          )}
+        </div>
+      </div>
+      <div className="project-view_specific-objectives_main-container">
+        {specificObjectives.map((specificObjective) => (
+          <ProjectSpecificObjectiveItemReadOnly {...specificObjective} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default ProjectSpecificObjectiveListReadOnly;
